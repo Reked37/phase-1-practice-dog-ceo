dd71f2495f05ff432887e302e55946c83671ed55
Derek Truesdale <dtruesdale37@gmail.com>
2023-05-09 16:11:36 -0500

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',()=>fetchDogs())
+
+function fetchDogs(){
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+     .then((res)=> res.json())
+     .then((json) => (json.message.forEach((element)=>{
+        const createli = document.createElement('img')
+        const dogContainer = document.querySelector('#dog-image-container')
+        createli.src = element
+        dogContainer.appendChild(createli)
+     })));   
+}
+    
+
